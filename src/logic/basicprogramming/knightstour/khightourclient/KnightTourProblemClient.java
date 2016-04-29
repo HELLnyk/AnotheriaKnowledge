9a14fc8c140115de4364941fe1a6b8208585f275
HELLnyk <yurock.ua@gmail.com>
2016-04-29 12:26:57 +0300

    Knight`s tour client (include main method for testing knight`s tour problem)

---
+++ src/logic/basicprogramming/knightstour/khightourclient/KnightTourProblemClient.java
@@ -0,0 +1,13 @@
+package logic.basicprogramming.knightstour.khightourclient;
+
+import logic.basicprogramming.knightstour.khightstoursolution.KnightTourProblem;
+
+/**
+ * @author hellnyk
+ */
+public class KnightTourProblemClient {
+
+    public static void main(String[] args) {
+        new KnightTourProblem().getResult(4, 6);
+    }
+}
