4b50efeae643b8c1b2950c77fa51290ed53ab11a
HELLnyk <yurock.ua@gmail.com>
2016-04-29 12:35:00 +0300

    testing MagicSquareProblem`s class

---
+++ src/logic/basicprogramming/magicsquares/msclient/MagicSquareClient.java
@@ -0,0 +1,13 @@
+package logic.basicprogramming.magicsquares.msclient;
+
+import logic.basicprogramming.magicsquares.mssolution.MagicSquareProblem;
+
+/**
+ * @author hellnyk
+ */
+public class MagicSquareClient {
+
+    public static void main(String[] args) {
+        new MagicSquareProblem(5).getResult();
+    }
+}
