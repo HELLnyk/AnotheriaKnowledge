f5c3fca31404d0eca6955dbaee9d3f6bc1671320
HELLnyk <yurock.ua@gmail.com>
2016-05-04 19:11:58 +0300

    were divided assembly and writing data

--- src/logic/basicprogramming/magicsquares/mssolution/FileWriter.java
+++ src/logic/basicprogramming/magicsquares/mssolution/FileWriter.java
@@ -10,19 +10,33 @@ public class FileWriter {
     private static final String FILE_NAME = "/home/hellnyk/HELLnyk/gitRepositories/AnotheriaKnowledge/matrixoutput.txt";
 
     public static void writeMatrix(int [][] matrix, int number){
-
         StringBuffer stringBuffer = new StringBuffer();
         String title = "**** " + number + " matrix ****\n";
         stringBuffer.append(title);
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 0; column < matrix[row].length; column++) {
-                String element = matrix[row][column] + " ";
+                String element;
+                if(matrix[row][column] < 10)
+                   element = matrix[row][column] + "  ";
+                else
+                   element = matrix[row][column] + " ";
+
                 stringBuffer.append(element);
             }
             stringBuffer.append("\n");
         }
         stringBuffer.append("\n");
+        printData(stringBuffer);
+    }
 
+    //------------------
+            public static void printValue(int value){
+                StringBuffer stringBuffer = new StringBuffer(value);
+                printData(stringBuffer);
+            }
+    //------------------
+
+    private static void printData(StringBuffer stringBuffer){
         try(RandomAccessFile file = new RandomAccessFile(FILE_NAME, "rw")) {
             file.skipBytes((int) file.length());
             file.writeBytes(stringBuffer.toString());
