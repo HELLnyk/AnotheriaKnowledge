02c98fd812245828f8827ac21d44f4149bbece9e
HELLnyk <yurock.ua@gmail.com>
2016-05-04 11:25:20 +0300

    wrote method to testing on position or all positions on chessboard of the way of knight

--- src/logic/basicprogramming/knightstour/khightourclient/KnightTourProblemClient.java
+++ src/logic/basicprogramming/knightstour/khightourclient/KnightTourProblemClient.java
@@ -8,6 +8,25 @@ import logic.basicprogramming.knightstour.khightstoursolution.KnightTourProblem;
 public class KnightTourProblemClient {
 
     public static void main(String[] args) {
-        new KnightTourProblem().getResult(4, 6);
+        //KnightTourProblemClient.onPosition(0,0);
+        KnightTourProblemClient.allResults();
+    }
+
+    public static void  allResults(){
+        int counter = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                counter++;
+                if(i == 5 && j == 4){
+                    continue;
+                }
+                System.out.println(String.format("Stage: %d Position (X: %d; Y:%d)", counter, i, j));
+                new KnightTourProblem().getResult(i, j);
+            }
+        }
+    }
+
+    public static void onPosition(int x, int y){
+        new KnightTourProblem().getResult(x, y);
     }
 }
