c882c23af6ec77b6079d06285cd352e6fec283b7
HELLnyk <yurock.ua@gmail.com>
2016-05-12 19:02:45 +0300

    factory for concrete realization data storage

--- src/threading/concasses/includes/FactoryDataStorage.java
+++ src/threading/concasses/includes/FactoryDataStorage.java
@@ -3,21 +3,34 @@ package threading.concasses.includes;
 import threading.concasses.catmone.DataStorageOne;
 import threading.concasses.catmthree.DataStorageThree;
 import threading.concasses.catmtwo.DataStorageTwo;
-import threading.concasses.includes.AbstractDataStorage;
+
+import java.util.Map;
 
 /**
+ * Factory for generation what type of {@link Map} will be used
+ *
  * @author hellnyk
  */
 public class FactoryDataStorage {
 
+    /**
+     * List types supported generator
+     */
     public static final int DATA_STORAGE_ONE = 1;
     public static final int DATA_STORAGE_TWO = 2;
     public static final int DATA_STORAGE_THREE = 3;
 
-
-    public static AbstractDataStorage getDataStorage(int typeOfDataStorege){
-        AbstractDataStorage dataStorage = null;
-        switch (typeOfDataStorege){
+    /**
+     * get concrete worker for {@link AbstractDataStorage} instance
+     *
+     * @param typeOfDataStorage
+     *      id of concrete realization {@link AbstractDataStorage} instance
+     * @return
+     *      concrete realization of {@link AbstractDataStorage} instance
+     */
+    public static AbstractDataStorage getDataStorage(int typeOfDataStorage){
+        AbstractDataStorage dataStorage;
+        switch (typeOfDataStorage){
             case DATA_STORAGE_ONE:
                 dataStorage = new DataStorageOne();
                 break;
