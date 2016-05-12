e0f4519b785d9a587bdc70547614a660f49dc3ab
HELLnyk <yurock.ua@gmail.com>
2016-05-12 18:55:05 +0300

    adding elements to Map with using ConcurrentHashMap

--- src/threading/concasses/catmthree/DataStorageThree.java
+++ src/threading/concasses/catmthree/DataStorageThree.java
@@ -2,14 +2,18 @@ package threading.concasses.catmthree;
 
 import threading.concasses.includes.AbstractDataStorage;
 
-import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ *Saver of information in the {@link ConcurrentHashMap} instance
+ *
  * @author hellnyk
  */
 public class DataStorageThree extends AbstractDataStorage<String, Integer> {
 
+    /**
+     * Default constructor for initialize instance
+     */
     public DataStorageThree(){
         map = new ConcurrentHashMap<>();
     }
