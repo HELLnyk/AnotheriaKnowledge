cd8874231af765a500451ef8937ba1eac0bb5a74
HELLnyk <yurock.ua@gmail.com>
2016-05-12 15:09:04 +0300

    write simple logs into the file

---
+++ src/threading/catmone/InformationToFile.java
@@ -0,0 +1,29 @@
+package threading.catmone;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Writer of text message for different files
+ *
+ * @author hellnyk
+ */
+public class InformationToFile {
+
+    /**
+     * write information to the file
+     *
+     * @param fileName
+     *      name of the file, where information will be written
+     * @param info
+     *      name of the message, where information will be written
+     */
+    public static void write(String fileName, String info){
+        try(RandomAccessFile file = new RandomAccessFile(fileName, "rw")) {
+            file.skipBytes((int) file.length());
+            file.writeBytes(info + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+}
