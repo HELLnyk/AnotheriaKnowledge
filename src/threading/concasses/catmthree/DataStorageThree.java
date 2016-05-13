7869e7df813413169f2a45213fbe35dddeec9ca5
HELLnyk <yurock.ua@gmail.com>
2016-05-13 13:57:20 +0300

    rewrite setter method of values to the Map

--- src/threading/concasses/catmthree/DataStorageThree.java
+++ src/threading/concasses/catmthree/DataStorageThree.java
@@ -2,18 +2,32 @@ package threading.concasses.catmthree;
 
 import threading.concasses.includes.AbstractDataStorage;
 
-import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * Saver of information in the {@link ConcurrentHashMap} instance
+ *
  * @author hellnyk
  */
 public class DataStorageThree extends AbstractDataStorage<Integer, String> {
 
+    /**
+     * Default constructor for initialize {@link java.util.Map} instance
+     */
     public DataStorageThree(){
         map = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Write information to the {@link ConcurrentHashMap} instance
+     *
+     * @param name
+     *      name of current thread, which writes information
+     * @param key
+     *      {@link Integer} instance of the key for writing
+     * @param value
+     *      {@link String} instance value for writing
+     */
     @Override
     public void getInfoToMap(String name, Integer key, String value) {
         super.getInfoToMap(name, key, value);
--- src/threading/concasses/catmtwo/DataStorageTwo.java
+++ src/threading/concasses/catmtwo/DataStorageTwo.java
@@ -1,25 +1,38 @@
 package threading.concasses.catmtwo;
 
 import threading.concasses.includes.AbstractDataStorage;
-import threading.concasses.includes.InformationToFile;
 
 import java.util.*;
 
 /**
+ * Saver of information in the synchronized {@link HashMap} instance
+ *
  * @author hellnyk
  */
-public class DataStorageTwo extends AbstractDataStorage<String, Integer> {
+public class DataStorageTwo extends AbstractDataStorage<Integer, String> {
 
+    /**
+     * Default constructor for initialize {@link java.util.Map} instance
+     */
     public DataStorageTwo(){
-        Map<String, Integer> hashMap = new HashMap<>();
+        Map<Integer, String> hashMap = new HashMap<>();
         map = Collections.synchronizedMap(hashMap);
     }
 
+    /**
+     * Write information to the synchronized {@link HashMap} instance
+     *
+     * @param name
+     *      name of current thread, which writes information
+     * @param key
+     *      {@link Integer} instance of the key for writing
+     * @param value
+     *      {@link String} instance value for writing
+     */
     @Override
-    public synchronized void getInfoToMap(String name, String key, Integer value) {
+    public synchronized void getInfoToMap(String name, Integer key, String value) {
         printResult("thread " + name + " (before)");
         map.put(key, value);
-        counterWriting++;
         printResult("thread " + name + " (after)");
         notifyAll();
     }
