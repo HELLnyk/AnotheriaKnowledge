4209be737a062537660a68ffd641fbbc8aff524b
HELLnyk <yurock.ua@gmail.com>
2016-05-13 14:10:58 +0300

    were swapped key and value, also were added generation of the creating values

--- src/threading/concasses/includes/Worker.java
+++ src/threading/concasses/includes/Worker.java
@@ -1,30 +1,38 @@
 package threading.concasses.includes;
 
-import threading.concasses.catmone.DataStorageOne;
-import threading.concasses.includes.AbstractDataStorage;
-
 import java.util.Random;
 
 /**
- * Worker, which writes information to the {@link DataStorageOne} instance
+ * Worker, which writes information to the {@link AbstractDataStorage} instance
  *
  * @author hellnyk
  */
 public class Worker implements Runnable {
 
     /**
-     * default keys for writing
+     * capacity of how mach values will be put
      */
-    public static final String[] DIFFERENT_KEYS = {
-            "keyOne",
-            "keyTwo",
-            "keyThree",
-            "keyFour",
-            "keyFive",
-    };
+    private static final int DIFFERENT_VALUES_AMOUNT = 100000;
 
     /**
-     * {@link DataStorageOne} instance
+     * default length of every value
+     */
+    private static final int DIFFERENT_VALUE_LENGTH = 100;
+
+    /**
+     * array of {@link String} values
+     */
+    private static final String[] DIFFERENT_VALUES;
+
+    /**
+     *initialization of values
+     */
+    static {
+        DIFFERENT_VALUES = generateString();
+    }
+
+    /**
+     * {@link AbstractDataStorage} instance
      */
     private AbstractDataStorage dataStorage;
 
@@ -42,7 +50,7 @@ public class Worker implements Runnable {
      * Default constructor
      *
      * @param dataStorage
-     *      {@link DataStorageOne} instance, which will be used by this worker
+     *      {@link AbstractDataStorage} instance, which will be used by this worker
      * @param nameId
      *      name of worker
      */
@@ -54,15 +62,31 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
-        for(String key: DIFFERENT_KEYS){
-
-            dataStorage.getInfoToMap(name, key, setInfo());
-            try {
-                Thread.sleep(1000);
-            }catch (InterruptedException e){
-                throw new RuntimeException();
-            }
+        int counter = 1;
+        for(String key: DIFFERENT_VALUES){
+            dataStorage.getInfoToMap(name, counter, key);
+            counter++;
         }
+    }
+
+    /**
+     * generate different {@link String} values
+     *
+     * @return
+     *      array of {@link String} values
+     */
+    private static String[] generateString(){
+        String[] mass = new String[DIFFERENT_VALUES_AMOUNT];
+        Random random = new Random();
+        String elements = "ABCDEFGHIJKLMNOPQRSTUYWXVZ";
+        for (int key = 0; key < DIFFERENT_VALUES_AMOUNT; key++) {
+            char massChar[] = new char[DIFFERENT_VALUE_LENGTH];
+            for (int letter = 0; letter < DIFFERENT_VALUE_LENGTH; letter++) {
+                massChar[letter] = elements.charAt(random.nextInt(elements.length()));
+            }
+            mass[key] = new String(massChar);
+        }
+        return mass;
     }
 
     /**
