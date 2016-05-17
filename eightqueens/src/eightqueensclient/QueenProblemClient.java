f925b38dac26fe151ad8f276690b62eaf9111078
HELLnyk <yurock.ua@gmail.com>
2016-05-17 12:57:35 +0300

    was changed method to write parameter in constructor of queens problem main class

--- eightqueens/src/eightqueensclient/QueenProblemClient.java
+++ eightqueens/src/eightqueensclient/QueenProblemClient.java
@@ -10,6 +10,6 @@ import eightqueensolution.QueenProblem;
 public class QueenProblemClient {
 
     public static void main(String[] args) {
-        new QueenProblem(8, 0).searchResult();
+        new QueenProblem(8, QueenProblem.PRINT_VECTOR).searchResult();
     }
 }
