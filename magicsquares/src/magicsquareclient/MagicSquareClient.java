2cf48e6984165577bb243b788b411fdbbd5558db
HELLnyk <yurock.ua@gmail.com>
2016-05-19 15:28:47 +0300

    was modified capacity of square

--- magicsquares/src/magicsquareclient/MagicSquareClient.java
+++ magicsquares/src/magicsquareclient/MagicSquareClient.java
@@ -1,20 +1,16 @@
 package magicsquareclient;
 
 import mssolution.MagicSquareProblem;
-import mssolution.Reshuffle;
-import mssolution.MSProblem;
-
-import java.util.Arrays;
 
 /**
  * @author hellnyk
  */
 public class MagicSquareClient {
 
-    private static final int DEFAULT_CAPACITY_OF_SQUARE = 5;
+    private static final int DEFAULT_CAPACITY_OF_SQUARE = 4;
 
     public static void main(String[] args) {
-       new MSProblem(DEFAULT_CAPACITY_OF_SQUARE).findResult();
+        new MagicSquareProblem(DEFAULT_CAPACITY_OF_SQUARE).getSomeResult();
     }
 }
 
