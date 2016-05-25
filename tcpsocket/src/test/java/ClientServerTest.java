f1b44f26c03f41d4af8a07cb8f25179967bf297b
HELLnyk <yurock.ua@gmail.com>
2016-05-25 14:43:03 +0300

    test tcp  socket application

---
+++ tcpsocket/src/test/java/ClientServerTest.java
@@ -0,0 +1,38 @@
+import client.SimpleClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import server.SimpleServer;
+
+/**
+ * @author hellnyk
+ */
+public class ClientServerTest {
+
+    @Before
+    public void testServerStart(){
+        new Thread(new Runnable() {
+            public void run() {
+                SimpleServer.runServer();
+            }
+        }).start();
+    }
+
+    @After
+    public void testServerSop(){
+       new Thread(new Runnable() {
+           public void run() {
+               SimpleServer.stopServer();
+           }
+       }).start();
+    }
+
+    @Test
+    public void testWork(){
+        String[] elements  = {"test", "element", "value", "string", "anotheria"};
+        for(String element: elements){
+            SimpleClient.main(new String[]{element});
+        }
+    }
+
+}
