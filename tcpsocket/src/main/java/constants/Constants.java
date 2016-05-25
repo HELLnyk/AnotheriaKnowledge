e2d16c1c1cc89ce018352c343a32ce9b892999d2
HELLnyk <yurock.ua@gmail.com>
2016-05-25 14:42:23 +0300

    constant values for connect to tcp socket  and for reading information

---
+++ tcpsocket/src/main/java/constants/Constants.java
@@ -0,0 +1,34 @@
+package constants;
+
+/**
+ * Class of some constants
+ *
+ * @author hellnyk
+ */
+public class Constants {
+
+    /**
+     * value of bytes
+     */
+    public static final int BYTES = 1024;
+
+    /**
+     * value of kbytes
+     */
+    public static final int KB_BYTES = 64;
+
+    /**
+     * port number
+     */
+    public static final int PORT = 4000;
+
+    /**
+     *time delay for socket
+     */
+    public static final int TIMEOUT = 0;
+
+    /**
+     * host name
+     */
+    public static final String NAME_HOST = "localhost";
+}
