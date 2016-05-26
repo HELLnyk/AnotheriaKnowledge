599e9dd5ea4292cd0b73e1d77cbad631a1c04946
HELLnyk <yurock.ua@gmail.com>
2016-05-26 15:08:31 +0300

    test UDP

---
+++ udpsocket/src/test/java/UDPTest.java
@@ -0,0 +1,27 @@
+import client.UDPSimpleClient;
+import org.junit.Before;
+import org.junit.Test;
+import server.UDPSimpleServer;
+
+/**
+ * @author hellnyk
+ */
+public class UDPTest {
+
+
+    @Before
+    public void testStartUPDServer(){
+        new Thread(()-> {
+            UDPSimpleServer.main(new String[]{});
+        }).start();
+    }
+
+    @Test
+    public void testConnection(){
+        String[] ss = {"anotheria", "test", "Lorem ipsum"};
+        //UDPSimpleClient.main(new String[]{ss[0]});
+        for (String s: ss) {
+            UDPSimpleClient.main(new String[]{s});
+        }
+    }
+}
