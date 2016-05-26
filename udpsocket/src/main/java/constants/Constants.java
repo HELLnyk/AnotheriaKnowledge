98da39b1c46fbc5a67b998bd690e3c76f2ab9118
HELLnyk <yurock.ua@gmail.com>
2016-05-26 15:07:08 +0300

    default parameters of constants for the socket

---
+++ udpsocket/src/main/java/constants/Constants.java
@@ -0,0 +1,24 @@
+package constants;
+
+/**
+ * Some constants
+ *
+ * @author hellnyk
+ */
+public class Constants {
+
+    /**
+     * size of data array
+     */
+    public static final int BUFFER_SIZE = 2048;
+
+    /**
+     * port number
+     */
+    public static final int PORT = 9999;
+
+    /**
+     * host name
+     */
+    public static final String HOST = "127.0.0.1";
+}
