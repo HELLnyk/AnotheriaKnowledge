ddf6bfc8acccc25f2b4db7a37a24257fcaf960b4
HELLnyk <yurock.ua@gmail.com>
2016-05-31 15:38:42 +0300

    POJO - some unit of data

--- tcpfiletransfermanager/src/main/java/data/DataEvent.java
+++ tcpfiletransfermanager/src/main/java/data/DataEvent.java
@@ -3,20 +3,39 @@ package data;
 import java.io.Serializable;
 
 /**
+ * Unit of data
+ *
  * @author hellnyk
  */
 public class DataEvent implements Serializable {
 
 
+    /**
+     * serialVersionUID
+     */
     private static final long serialVersionUID = -7408423763713307818L;
 
+    /**
+     * some parameters of POJO
+     */
     private int id;
     private String name;
 
+    /**
+     * Default constructor for initialization parameters
+     *
+     * @param name
+     * @param id
+     */
     public DataEvent(String name, int id) {
         this.name = name;
         this.id = id;
     }
+
+    /**
+     * getters and setters
+     */
+
 
     public int getId() {
         return id;
