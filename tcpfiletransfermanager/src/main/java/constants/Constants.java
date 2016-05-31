fd5a6e3f7c5bb16725255820846e7eaddf2055af
HELLnyk <yurock.ua@gmail.com>
2016-05-31 15:24:34 +0300

    default constants

---
+++ tcpfiletransfermanager/src/main/java/constants/Constants.java
@@ -0,0 +1,29 @@
+package constants;
+
+/**
+ * Default constants values
+ *
+ * @author hellnyk
+ */
+public class Constants {
+
+    /**
+     * main port number for commands
+     */
+    public static final int DEFAULT_COMMAND_PORT = 6666;
+
+    /**
+     * main port number for data transfer
+     */
+    public static final int DEFAULT_DATA_TRANSFER_PORT = 7777;
+
+    /**
+     * default host
+     */
+    public static final String DEFAULT_HOST = "localhost";
+
+    /**
+     * default size of data array <code>int</code>  for command
+     */
+    public static final int BYTES = 1024;
+}
