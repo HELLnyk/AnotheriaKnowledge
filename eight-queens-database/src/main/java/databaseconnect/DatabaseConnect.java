dd264a11381bfe9a86058124695e4c5ed14bbb2b
HELLnyk <yurock.ua@gmail.com>
2016-06-16 15:11:26 +0300

    type of connection to the database

---
+++ eight-queens-database/src/main/java/databaseconnect/DatabaseConnect.java
@@ -0,0 +1,19 @@
+package databaseconnect;
+
+/**
+ * type of connection
+ *
+ * @author hellnyk
+ */
+public enum DatabaseConnect {
+
+    /**
+     * Connection with jdbc
+     */
+    JDBC,
+
+    /**
+     * Connection with hibernate
+     */
+    HIBERNATE
+}
